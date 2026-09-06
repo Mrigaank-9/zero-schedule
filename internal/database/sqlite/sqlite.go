f3f3a07e081543f4ffed2beb5fc8129ddb17bf30
mrigaank-9 <mrigaankmj@gmail.com>
2026-09-06 20:09:16 +0530

    updated the jobs struct and updated sqlite wrapper

--- internal/database/sqlite/sqlite.go
+++ internal/database/sqlite/sqlite.go
@@ -10,7 +10,7 @@ import (
 )
 
 type Sqlite struct {
-	Db *sql.DB
+	db *sql.DB
 }
 
 func SetupSQL(uri string) database.Database {
@@ -18,29 +18,29 @@ func SetupSQL(uri string) database.Database {
 	if err != nil {
 		log.Fatal("Unable to create the DB object : ", err)
 	}
-	return &Sqlite{Db: db}
+	return &Sqlite{db: db}
 }
 
 func (s *Sqlite) Exec(ctx context.Context, query string, args ...any) (sql.Result, error) {
-	return s.Db.ExecContext(ctx, query, args...)
+	return s.db.ExecContext(ctx, query, args...)
 }
 
 func (s *Sqlite) Query(ctx context.Context, query string, args ...any) (*sql.Rows, error) {
-	return s.Db.QueryContext(ctx, query, args...)
+	return s.db.QueryContext(ctx, query, args...)
 }
 
 func (s *Sqlite) QueryRow(ctx context.Context, query string, args ...any) *sql.Row {
-	return s.Db.QueryRowContext(ctx, query, args...)
+	return s.db.QueryRowContext(ctx, query, args...)
 }
 
 func (s *Sqlite) PrepareContext(ctx context.Context, query string) (*sql.Stmt, error) {
-	return s.Db.PrepareContext(ctx, query)
+	return s.db.PrepareContext(ctx, query)
 }
 
 func (s *Sqlite) BeginTx(ctx context.Context, opts *sql.TxOptions) (*sql.Tx, error) {
-	return s.Db.BeginTx(ctx, opts)
+	return s.db.BeginTx(ctx, opts)
 }
 
 func (s *Sqlite) Close() error {
-	return s.Db.Close()
+	return s.db.Close()
 }
---
+++ internal/types/jobs.go
@@ -0,0 +1,39 @@
+package types
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type JobStatus string
+
+const (
+	QUEUED  JobStatus = "QUEUED"
+	RUNNING JobStatus = "RUNNING"
+	DONE    JobStatus = "DONE"
+	FAILED  JobStatus = "FAILED"
+)
+
+type Job struct {
+	JobID     uuid.UUID
+	Name      string
+	Command   string
+	Status    JobStatus
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	StartedAt *time.Time
+	EndedAt   *time.Time
+}
+
+func NewJob(name string, command string) *Job {
+	job := Job{
+		JobID:     uuid.New(),
+		Command:   command,
+		Name:      name,
+		Status:    QUEUED,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+	return &job
+}
